a86c6c7814dab95d55a9c383d0bb53899d9189f3
joerhew <joerhew@gmail.com>
2023-02-07 15:51:08 -0500

    Added settings and made the page responsive

--- js-file.js
+++ js-file.js
@@ -1,29 +1,108 @@
-var rows = [];
-var cells = [];
-var height = 16;
-var width = 16;
+const DEFAULT_LENGTH = 10;
+const DEFAULT_BRUSH_STRENGTH = 0.1;
+const DEFAULT_MODE = 'Black & White';
 
-let sketchpad = document.querySelector('.sketchpad');
+let length = DEFAULT_LENGTH;
+let brushStrength = DEFAULT_BRUSH_STRENGTH;
+let mode = DEFAULT_MODE;
 
-for (var i = 1; i <= height; i++) {
-    rows[i] = document.createElement('div');
-    rows[i].classList.toggle('row');
-    rows[i].setAttribute('id','r'+i);
-    sketchpad.appendChild(rows[i]);  
+// Settings
 
-    for (var j = 1; j <= width; j++) {
-        cells[j] = document.createElement('div');
-        cells[j].classList.toggle('cell');
-        cells[j].setAttribute('id','r'+i+'c'+j);
-        rows[i].appendChild(cells[j]);
+const settings = document.querySelector('.settings');
+const lengthValue = document.querySelector('#length-value');
+const lengthSlider = document.querySelector('#length-slider');
+const brushStrengthValue = document.querySelector('#brush-strength');
+const brushStrengthSlider = document.querySelector('#brush-strength-slider');
+const radioButtons = document.querySelectorAll('input[name="mode"]');
+const modeValue = document.querySelector('#mode-value');
+const sketchpad = document.querySelector('.sketchpad');
+const resetButton = document.querySelector('.reset-button');
+
+lengthSlider.addEventListener('change', () => {
+    wipeSketchpad();
+    lengthSlider.setAttribute('value',lengthSlider.value);
+    length = lengthSlider.getAttribute('value');
+    lengthValue.textContent = length;
+    createSketchpad(length);
+});
+
+brushStrengthSlider.addEventListener('change', () => {
+    brushStrengthSlider.setAttribute('value', brushStrengthSlider.value);
+    brushStrength = parseFloat(brushStrengthSlider.getAttribute('value'));
+    brushStrengthValue.textContent = brushStrength;
+})
+
+radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener('change', () => {
+        mode = radioButton.value;
+        modeValue.textContent = mode;
+    })
+}) 
+
+resetButton.addEventListener('click', () => {
+    wipeSketchpad();
+    createSketchpad(length);
+})
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function createSketchpad(length) {
+    sketchpad.style.gridTemplateColumns = `repeat(${length}, 1fr)`
+    sketchpad.style.gridTemplateRows = `repeat(${length}, 1fr)`
+
+    for (let i = 0; i < length * length; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.toggle('grid-element');
+        sketchpad.appendChild(gridElement);  
+        gridElement.addEventListener('mouseover', changeColour);
+        gridElement.addEventListener('mousedown', changeColour);
+        sketchpad.appendChild(gridElement);
     };
+    
+    lengthValue.textContent = length + " x " + length;
+
+    const cls = document.querySelectorAll('.cell'); 
+
 };
 
-const cls = document.querySelectorAll('.cell'); 
+function changeColour(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if (mode === 'Eraser') {
+        e.target.style.backgroundColor = '';
+    } else if (mode === 'Black & White' && e.target.style.backgroundColor === '') {
+        e.target.style.backgroundColor = getBlackWhiteColor();
+        e.target.style.opacity = (parseFloat(e.target.style.opacity) || 0) + brushStrength;
+    } else if (mode === 'Pastel' && e.target.style.backgroundColor === '') {
+        e.target.style.backgroundColor = getPastelColor();
+        e.target.style.opacity = (parseFloat(e.target.style.opacity) || 0) + brushStrength;
+    } else {
+        e.target.style.opacity = (parseFloat(e.target.style.opacity) || 0) + brushStrength;
+    }
+    
+}
 
-cls.forEach((cl) => {
-    cl.addEventListener('mouseover', () => { //change to mouseover
-        cl.style.backgroundColor = '#000000';
-        cl.style.opacity = (parseFloat(cl.style.opacity) || 0) + 0.05;
-    });
-});
+function getPastelColor(){ 
+    return "hsl(" + 360 * Math.random() + ',' +
+               (25 + 70 * Math.random()) + '%,' + 
+               (85 + 10 * Math.random()) + '%)'
+  } 
+
+function getBlackWhiteColor(){
+    return "hsl(" + 0 + ',' +
+                    0 + '%,' + 
+                    100 * (Math.random()) + '%)'
+  }
+
+function wipeSketchpad() {
+    sketchpad.innerHTML = '';
+}
+
+window.onload = () => {
+    createSketchpad(length);
+    brushStrengthValue.textContent = brushStrength;
+    modeValue.textContent = mode;
+    document.getElementById("black-white").checked = true;
+}
